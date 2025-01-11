8574da3ffb1d88adc1bcb48630761f3f7285683b
Xenthio <thegooster88@gmail.com>
2025-01-11 13:48:22 +1100

    new gamer culling fixes very epic

--- source/culling_fixes.cpp
+++ source/culling_fixes.cpp
@@ -32,12 +32,20 @@ Define_method_Hook(bool, CViewRenderShouldForceNoVis, void*)
 	return original;
 }
 
+Define_method_Hook(bool, MathLibR_CullBox, void*, const Vector& mins, const Vector& maxs, const Frustum_t& frustum)
+{
+	return false;
+	//return MathLibR_CullBox_trampoline()(_this, mins, maxs, frustum);
+}
+
 static StudioRenderConfig_t s_StudioRenderConfig;
  
 void CullingHooks::Initialize() {
 	try {
 		auto client = GetModuleHandle("client.dll");
+		auto engine = GetModuleHandle("engine.dll");
 		if (!client) { Msg("client.dll == NULL\n"); }
+		if (!engine) { Msg("engine.dll == NULL\n"); }
 
 		//static const char sign1[] = "44 8B B1 ? ? ? ? 48 89 55";
 		//auto CViewRenderRender = ScanSign(client, sign1, sizeof(sign1) - 1);
@@ -56,11 +64,20 @@ void CullingHooks::Initialize() {
 
 		static const char sign2[] = "0F B6 81 54 03 00 00 C3";
 		auto CViewRenderShouldForceNoVis = ScanSign(client, sign2, sizeof(sign2) - 1);
-		if (!CViewRenderShouldForceNoVis) { Msg("[Culling Fixes] CViewRender::ShouldForceNoVis == NULL\n"); return; }
+		if (!CViewRenderShouldForceNoVis) { Msg("[Culling Fixes] CViewRender::ShouldForceNoVis == NULL\n"); }
 		else {
 			Msg("[Culling Fixes] Hooked CViewRender::ShouldForceNoVis\n");
 			Setup_Hook(CViewRenderShouldForceNoVis, CViewRenderShouldForceNoVis)
 		}
+
+		static const char sign3[] = "48 83 EC 48 0F 10 22";
+		auto CLIENT_R_CullBox = ScanSign(client, sign3, sizeof(sign3) - 1);
+		auto ENGINE_R_CullBox = ScanSign(engine, sign3, sizeof(sign3) - 1);
+		if (!CLIENT_R_CullBox) { Msg("[Culling Fixes] MathLib (CLIENT) R_CullBox == NULL\n"); }
+		else { Msg("[Culling Fixes] Hooked MathLib (CLIENT) R_CullBox\n"); Setup_Hook(MathLibR_CullBox, CLIENT_R_CullBox) }
+
+		if (!ENGINE_R_CullBox) { Msg("[Culling Fixes] MathLib (ENGINE) R_CullBox == NULL\n"); }
+		else { Msg("[Culling Fixes] Hooked MathLib (ENGINE) R_CullBox\n"); Setup_Hook(MathLibR_CullBox, ENGINE_R_CullBox) }
 	}
 	catch (...) {
 		Msg("[Culling Fixes] Exception in CullingHooks::Initialize\n");
@@ -71,6 +88,7 @@ void CullingHooks::Shutdown() {
 	// Existing shutdown code  
 	//CViewRenderRender_hook.Disable();
 	CViewRenderShouldForceNoVis_hook.Disable();
+	MathLibR_CullBox_hook.Disable();
 
 	// Log shutdown completion
 	Msg("[Culling Fixes] Shutdown complete\n");
