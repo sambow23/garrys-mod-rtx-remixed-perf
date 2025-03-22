e235ca0d505ef537fd8448238a848e48426fde42
Xenthio <thegooster88@gmail.com>
2025-03-22 23:38:57 +1100

    new patches to possibly maybe make more models have stable hashes
    or also maybe crash the game who knows its a mystery update how fun!

--- source/prop_fixes.cpp
+++ source/prop_fixes.cpp
@@ -5,6 +5,7 @@
 #include "materialsystem/materialsystem_config.h"
 #include "interfaces/interfaces.h"  
 #include "prop_fixes.h"  
+#include <memory_patcher.h>
 
 using namespace GarrysMod::Lua;
 
@@ -30,8 +31,8 @@ void ModelRenderHooks::Initialize() {
 		auto studiorenderdll = GetModuleHandle("studiorender.dll");
 		if (!studiorenderdll) { Msg("studiorender.dll == NULL\n"); }
 
-		static const char sign[] = "48 89 54 24 10 48 89 4C 24 08 55 56 57 41 54 41 55 41 56 41 57 48 83 EC 50 48 8B 41 08 45 32 F6 49 63 F0 4D 8B E1 4C 8B EA 4C 8B F9 0F B6 A8 58 02 00 00 48 8B B8 50 02 00 00 40 88 AC 24 A0 00 00 00 83 FE 1F 77 20 4C 8B 84 F0 60 02 00 00 4D 85 C0 74 13 0F B6";
-		auto R_StudioSetupSkinAndLighting = ScanSign(studiorenderdll, sign, sizeof(sign) - 1);
+		static const char R_StudioSetupSkinAndLighting_sign[] = "48 89 54 24 10 48 89 4C 24 08 55 56 57 41 54 41 55 41 56 41 57 48 83 EC 50 48 8B 41 08 45 32 F6 49 63 F0 4D 8B E1 4C 8B EA 4C 8B F9 0F B6 A8 58 02 00 00 48 8B B8 50 02 00 00 40 88 AC 24 A0 00 00 00 83 FE 1F 77 20 4C 8B 84 F0 60 02 00 00 4D 85 C0 74 13 0F B6";
+		auto R_StudioSetupSkinAndLighting = ScanSign(studiorenderdll, R_StudioSetupSkinAndLighting_sign, sizeof(R_StudioSetupSkinAndLighting_sign) - 1);
 
 		if (!R_StudioSetupSkinAndLighting) { Msg("R_StudioSetupSkinAndLighting == NULL\n"); return; }
 
@@ -49,6 +50,45 @@ void ModelRenderHooks::Initialize() {
 		s_StudioRenderConfig.bDrawTangentFrame = false;
 		s_StudioRenderConfig.bFlex = false;
 		g_pStudioRender->UpdateConfig(s_StudioRenderConfig);  
+
+		HMODULE studiorenderModule = GetModuleHandleEx("studiorender.dll");
+
+		// hardware skin patch 1, override pColorMeshes 
+		// can fatally crash, disabled
+		
+		//g_MemoryPatcher.FindAndPatch(
+		//	"ForceHardwareSkinning1",
+		//	studiorenderModule,
+		//	"75 ?? 48 8B 41 ?? F6 40",
+		//	"90",
+		//	"Force models to use Hardware Skinning (1/2)"
+		//);
+
+		// hardware skin patch 2, overrides the first jnz to jump after pColorMeshes is checked
+		g_MemoryPatcher.FindAndPatch(
+			"ForceHardwareSkinning2",
+			studiorenderModule,
+			"75 ?? F6 40 ?? ?? 75",
+			"EB",
+			"Force models to use Hardware Skinning (2/2)"
+		);
+
+		g_MemoryPatcher.FindAndPatch(
+			"ForceStaticModel1",
+			studiorenderModule,
+			"75 ?? 84 C0 75",
+			"90",
+			"Force models to use static meshes (1/2)"
+		);
+
+		//g_MemoryPatcher.FindAndPatch(
+		//	"ForceStaticModel2",
+		//	studiorenderModule,
+		//	"75 ?? 8B 85 ?? ?? ?? ?? 33 F6",
+		//	"90",
+		//	"Force models to use static meshes (2/2)"
+		//);
+
 	}
 	catch (...) {
 		Msg("[Prop Fixes] Exception in ModelRenderHooks::Initialize\n");
@@ -58,6 +98,8 @@ void ModelRenderHooks::Initialize() {
 void ModelRenderHooks::Shutdown() { 
 	// Existing shutdown code  
 	R_StudioSetupSkinAndLighting_hook.Disable();
+	g_MemoryPatcher.DisablePatch("ForceHardwareSkinning1");
+	g_MemoryPatcher.DisablePatch("ForceHardwareSkinning2");
 
 	// Log shutdown completion
 	Msg("[Prop Fixes] Shutdown complete\n");
